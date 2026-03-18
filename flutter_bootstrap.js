61dc5763ea0b6bd178060f2559d14d9637c70a72
thejasenapps <thejasenapps@users.noreply.github.com>
2026-03-18 09:31:59 +0000

    deploy: 6a1f6981894843ef909200b77e2d7d389b259af0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "539243392" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1774214859" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
